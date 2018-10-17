211f8b436bd223b8b19ea0535b8850a59ff5b4ab
Kwabena W. Agyeman <kwagyeman@live.com>
2018-10-16 21:30:57 -0700

    Chars 74K nets

---
+++ scripts/examples/25-Machine-Learning/nn_cnrpark.py
@@ -0,0 +1,38 @@
+# CNRPark Parking Lot Detection CNN Example
+#
+# This example shows off how to use the OpenMV Cam to detect cars in
+# parking spaces outside by training a CNN for the OpenMV Cam on the
+# CNRPark Dataset.
+
+import sensor, image, time, os, nn
+
+sensor.reset()                         # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565
+sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
+sensor.set_windowing((128, 128))       # Set 128x128 window.
+sensor.skip_frames(time=500)
+sensor.set_auto_gain(False)
+sensor.set_auto_exposure(False)
+
+# Load cnrpark network
+net = nn.load('/cnrparkext.network')
+# Less accurate.
+# net = nn.load('/cnrpark.network')
+labels = ['busy', 'free']
+
+clock = time.clock()                # Create a clock object to track the FPS.
+while(True):
+    clock.tick()                    # Update the FPS clock.
+    img = sensor.snapshot()         # Take a picture and return the image.
+    out = net.forward(img, softmax=True)
+    print(out)
+    max_idx = out.index(max(out))
+    score = int(out[max_idx]*100)
+    if (score < 70):
+        score_str = "??:??%"
+    else:
+        score_str = "%s:%d%% "%(labels[max_idx], score)
+    img.draw_string(0, 0, score_str, color=(255, 0, 0))
+
+    print(clock.fps())             # Note: OpenMV Cam runs about half as fast when connected
+                                   # to the IDE. The FPS should increase once disconnected.
