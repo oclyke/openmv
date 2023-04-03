292a19ae6e87a502543a6110e6db3ba07dbac761
Kwabena W. Agyeman <kwagyeman@live.com>
2023-04-03 13:52:55 -0700

    Test performance diff

--- src/omv/boards/OPENMV4P/omv_boardconfig.h
+++ src/omv/boards/OPENMV4P/omv_boardconfig.h
@@ -144,16 +144,18 @@
 #define OMV_STACK_MEMORY        ITCM        // stack memory
 #define OMV_DMA_MEMORY          SRAM3       // Misc DMA buffers memory.
 #define OMV_FB_MEMORY           DRAM        // Framebuffer, fb_alloc
+#define OMV_HEAP_MEMORY         DRAM
+#define OMV_HEAP_MEMORY_OFFSET  (30M)
 #define OMV_JPEG_MEMORY         DRAM        // JPEG buffer memory buffer.
 #define OMV_JPEG_MEMORY_OFFSET  (31M)       // JPEG buffer is placed after FB/fballoc memory.
 #define OMV_VOSPI_MEMORY        SRAM4       // VoSPI buffer memory.
 #define OMV_FB_OVERLAY_MEMORY   AXI_SRAM    // Fast fb_alloc memory.
 
 #define OMV_FB_SIZE             (20M)       // FB memory: header + VGA/GS image
-#define OMV_FB_ALLOC_SIZE       (11M)       // minimum fb alloc size
-#define OMV_FB_OVERLAY_SIZE     (496*1024)  // Fast fb_alloc memory size.
-#define OMV_STACK_SIZE          (64K)
-#define OMV_HEAP_SIZE           (240K)
+#define OMV_FB_ALLOC_SIZE       (10M)       // minimum fb alloc size
+#define OMV_FB_OVERLAY_SIZE     (460*1024)  // Fast fb_alloc memory size.
+#define OMV_STACK_SIZE          (32K)
+#define OMV_HEAP_SIZE           (1M)
 #define OMV_SDRAM_SIZE          (32 * 1024 * 1024) // This needs to be here for UVC firmware.
 
 #define OMV_LINE_BUF_SIZE       (11 * 1024) // Image line buffer round(2592 * 2BPP * 2 buffers).
