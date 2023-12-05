2a0639fdf9763668e47e4576552826a2ccff2a0f
oclyke <oclyke@oclyke.dev>
2023-12-05 11:17:16 -1000

    split ap0202at and ar0147 implemnetations
    * creates a public API for controlling the AP0202AT ISP
    * employs the API to handle AR0147 specific reset actions

---
+++ src/omv/sensors/ap0202at_ar0147.h
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the OpenMV project.
+ *
+ * Copyright (c) 2023 oclyke <oclyke@oclyke.dev>
+ *
+ * This work is licensed under the MIT license, see the file LICENSE for details.
+ *
+ * AR0147 w/ AP0202AT ISP driver.
+ */
+#ifndef __AP0202AT_AR0147_H__
+#define __AP0202AT_AR0147_H__
+
+#include "openmv/src/omv/sensors/ap0202at.h"
+
+int ap0202at_ar0147_init(sensor_t *sensor);
+
+#endif // __AP0202AT_AR0147_H__
