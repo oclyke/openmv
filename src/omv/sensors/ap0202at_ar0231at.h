271849f9ee45d81779920c0922cb62859c744dad
oclyke <oclyke@oclyke.dev>
2024-01-25 15:54:08 -0800

    clean up ar0147 and ar0231at

--- src/omv/sensors/ap0202at_ar0231at.h
+++ src/omv/sensors/ap0202at_ar0231at.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the OpenMV project.
  *
- * Copyright (c) 2023 oclyke <oclyke@oclyke.dev>
+ * Copyright (c) 2023-2024 oclyke <oclyke@oclyke.dev>
  *
  * This work is licensed under the MIT license, see the file LICENSE for details.
  *
@@ -10,7 +10,7 @@
 #ifndef __AP0202AT_AR0231AT_H__
 #define __AP0202AT_AR0231AT_H__
 
-#include "openmv/src/omv/sensors/ap0202at.h"
+#include "ap0202at.h"
 
 int ap0202at_ar0231at_init(sensor_t *sensor);
 
